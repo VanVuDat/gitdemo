5bed6af37ecbb4dcefd3dcd63fdad49dda69af7e
Dat Van <finnalj3@gmail.com>
2016-11-02 17:26:24 +0700

    init project

---
+++ gitdemo/src/gitdemo.java
@@ -0,0 +1,28 @@
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+
+/**
+ * 
+ */
+
+/**
+ * @author US
+ *
+ */
+public class gitdemo extends JFrame {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		gitdemo  j  =  new gitdemo();
+		j.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		j.setVisible(true);
+		j.setPreferredSize(new Dimension(500,500));
+		j.pack();
+		j.setLayout(null);
+	}
+
+}
